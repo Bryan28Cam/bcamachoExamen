1481ac0323ca7f86716a08326134ab6403384ebe
master <master@SMLAB306PC13_23>
2023-06-21 22:04:09 -0500

    Agregar archivos de proyecto.

---
+++ bcamachoExamen/Registro.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace bcamachoExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        string usuario;
+        public Registro(string usuario)
+        {
+            InitializeComponent();
+            lblusuario.Text = "USUARIO CONECTADO: " + usuario;
+            this.usuario = usuario;
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnResumen_Clicked(object sender, EventArgs e)
+        {
+            String nombre = (txtNombre.Text);
+            String apellido = (txtApellido.Text);
+            String edad = (txtEdad.Text);
+            string pais = pkPais.Items[pkPais.SelectedIndex];
+            string ciudad = pkCiudades.Items[pkCiudades.SelectedIndex];
+            string montoinicial = (txtMontoI.Text);
+            string montomensual = (txtMontoM.Text);
+
+            Navigation.PushAsync( new Resumen(usuario, nombre, apellido, edad, pais, ciudad, montoinicial, montomensual));
+        }
+    }
+}
---
+++ bcamachoExamen/Resumen.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace bcamachoExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resumen : ContentPage
+    {
+        public Resumen(string usuario, string nombre, string apellido, string edad, string pais, string ciudad, string montoinicial, string montomensual)
+        {
+            InitializeComponent();
+            lblusuario.Text = "USUARIO CONECTADO: " + usuario;
+            txtNombre.Text = nombre;
+            txtApellido.Text = apellido;
+            txtEdad.Text = edad;
+            txtPais.Text = pais;
+            txtCiudad.Text = ciudad;
+            txtMontoI.Text = montoinicial;
+            txtMontoM.Text = montomensual;
+        }
+
+        private void btnSalir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
